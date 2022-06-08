faac359fbdef64b902b3875c8ba3278db583c6f6
aftab-jigger <aftabh150@gmail.com>
2022-06-08 18:28:38 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+// Built-in, Custom Middlwares & Third Part Middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log("Hi, I'm from middleware ");
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.requestTime);
+  next();
+});
+app.use(morgan('dev'));
+// ===========================================
+
+// ================= Routes =============================
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// ================= Start Server =============================
+const port = 8080;
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
