e8f3e42df6ff417f17d2c4ece413e4ce7c008240
aftab-jigger <aftabh150@gmail.com>
2022-06-10 15:43:21 +0500

    connected to DataBase

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
 
 console.log(process.env.NODE_ENV);
 // process is core module
 const port = process.env.PORT;
+
+// Connection with DB
+const DB = process.env.DATABASE; // here we're getting the string of DB connection
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful !');
+  });
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
